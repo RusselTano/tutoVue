4ca01c425b5479963569c87592f30dd1e8fb3fe2
Dylane <tanolamene@gmail.com>
2024-09-26 14:47:50 -0300

    Mise a jour

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Bonjour {{ firstname }}</h1>
+  <p>Counter : {{ count }}</p>
+  <button>Increment</button>
+  <button @click="increment">Increment2</button>
+  <button @click="decrement">Decrement</button>
+  <p>Comment allez vous ?</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref } from 'vue';
+const firstname = "Dylane";
+
+let count = ref(0);
+
+// setInterval(() => {
+//   count.value++;
+// }, 1000);
+
+function increment(){
+  count.value++;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+const decrement = () => {
+  count.value--;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
